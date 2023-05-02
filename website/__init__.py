fc10892b012b74ebd5b292bf054ab7f34937b881
mikastark7 <mikastark2606@gmail.com>
2023-05-02 20:07:06 +0100

    database
    group+users

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+#from .models import User
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -11,6 +11,7 @@ def create_website():
     website = Flask(__name__)
     website.config['SECRET_KEY'] = 'shalom korim li shomal'
     website.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+   # website.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
     db.init_app(website)
 
     from .views import views
@@ -19,7 +20,7 @@ def create_website():
     website.register_blueprint(views, url_prefix='/')
     website.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
+    
 
     with website.app_context():
         db.create_all()
@@ -33,3 +34,9 @@ def create_website():
         return User.query.get(int(user_id))
 
     return website
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+    name = db.Column(db.String(50))
--- website/models.py
+++ website/models.py
@@ -1,6 +1,12 @@
 from . import db
 from flask_login import UserMixin
 
+user_group= db.Table('user_group',
+     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+     db.Column('group_id', db.Integer, db.ForeignKey('group.id')))
+
+
+                     
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +14,18 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(50))
     name = db.Column(db.String(50))
     # is_admin = db.Column(db.Boolean(False))
+    participants= db.relationship('Group', secondary=user_group, backref='participant')
+
+   # def __repr__(self):
+       # return f'<User: {self.name}>'
+
+
+
+class Group(db.Model):
+    name= db.Column(db.String(50), nullable=False)
+    id= db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        return f'<Group: {self.name}>'
+
+
