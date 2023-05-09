89b095b060ed957054d66cb98b41027ece1cbf83
DanielBs1213 <112696847+DanielBs1213@users.noreply.github.com>
2023-05-09 21:10:40 +0300

    Update models.py

--- website/models.py
+++ website/models.py
@@ -6,11 +6,19 @@ user_group = db.Table('user_group',
                       db.Column('group_id', db.Integer, db.ForeignKey('group.id')))
 
 
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(50))
     name = db.Column(db.String(50))
+    notes = db.relationship('Note')
     participants = db.relationship('Group', secondary=user_group, backref='participant')
 
     def __repr__(self):
